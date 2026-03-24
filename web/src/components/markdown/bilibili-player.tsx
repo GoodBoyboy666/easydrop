d5fb2e67066c3f6a04b380bb93f02b85f49c9f28
GoodBoyboy <me@goodboyboy.top>
2026-03-24 17:11:47 +0800

    禁止自动播放bilibili视频

--- web/src/components/markdown/bilibili-player.tsx
+++ web/src/components/markdown/bilibili-player.tsx
@@ -21,11 +21,11 @@ export function BilibiliPlayer({ bvid }: BilibiliPlayerProps) {
     <div className="my-4 overflow-hidden rounded-xl border border-border/70 bg-card/60">
       <div className="aspect-video w-full">
         <iframe
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+          allow="accelerometer; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
           allowFullScreen
           className="size-full border-0"
           referrerPolicy="strict-origin-when-cross-origin"
-          src={`https://player.bilibili.com/player.html?bvid=${normalizedBvid}&page=1`}
+          src={`https://player.bilibili.com/player.html?bvid=${normalizedBvid}&page=1&autoplay=0`}
           title={`Bilibili 视频 ${normalizedBvid}`}
         />
       </div>
